d14da38124ce8482bdd697e9a7eb87b2cd62a944
zhangxi.qiu <zhangxi.qiu@mujin.com.cn>
2025-12-02 23:58:50 +0800

    00018-easy

--- playground/easy/00018-easy-huo-qu-yuan-zu-zhang-du.ts
+++ playground/easy/00018-easy-huo-qu-yuan-zu-zhang-du.ts
@@ -22,7 +22,10 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Length<T> = any
+// type Length<T> = any
+
+// Solution:
+type Length<T extends readonly any[]> = T['length']
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -45,3 +48,21 @@ type cases = [
   > 查看解答：https://tsch.js.org/18/solutions
   > 更多题目：https://tsch.js.org/zh-CN
 */
+
+// Note;
+// 知识点：
+//  索引访问类型 Indexed Access Types
+// https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html#handbook-content
+// type[Key] 语法：通过键名访问类型属性的类型
+// 键名key必须是字面量或联合类型：这里是字符串字面量 'length'
+
+// T['length'] 取元组的长度，length就是元祖所具有的属性，即key
+const test = ['123', '456'] as const
+type a = typeof test['length']
+// a = 2
+
+// 类似的 T[number] 取元组中的每个元素 获取联合类型
+// T[number] 表示：访问类型 T 的所有数字索引对应的值类
+const test2 = ['123', '456'] as const
+type b = typeof test2[number]
+// b = '123' | '456'
